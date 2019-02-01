818fe0156e8a6af4089ab98ffb11aa1329be334a
Jordan Reed <jordan.reed@infusionsoft.com>
2019-02-01 12:40:37 -0700

    stuff

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`hot-potate listening on port ${port}`))
+
+const game = {
+  users: {},
+  it: null,
+  countdown: 10,
+  loser: null
+};
+
+setInterval(() => {
+  const now = Date.now();
+
+  game.countdown--;
+  if (game.countdown <= 0) {
+    game.countdown = 10;
+    game.loser = game.it;
+    game.it = null;
+  } else if (game.countdown === 6) {
+    game.loser = null;
+  }
+
+  Object.keys(game.users).forEach(username => {
+    if (now - game.users[username] > 5000) {
+      delete game.users[username];
+      if (game.it === username) {
+        game.it = null;
+      }
+    }
+  });
+
+  if (!game.it) {
+    game.it = Object.keys(game.users)[0];
+  }
+}, 1000);
+
+app.post('/api/game', (req, res) => {
+  if (!req.body.username) {
+    return res.status(400).send();
+  }
+
+  game.users[req.body.username] = Date.now();
+  return res.status(201).send();
+});
+
+app.get('/api/game', (req, res) => {
+  res.json(game);
+});
+
+app.put('/api/game/it', (req, res) => {
+  if (!req.body.username) {
+    return res.status(400).send();
+  }
+
+  game.it = req.body.username;
+  return res.status(200).send();
+});
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,145 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div v-if="loading">loading...</div>
+    <div v-else class="users">
+      <div class="user" v-for="username in usernames" :key="username">
+        <img :src="`https://github.com/${username}.png`" @click="pass(username)" />
+        <div>{{ username }}</div>
+        <img v-if="gameState.it === username" src="https://media.giphy.com/media/3o7bu4EJkrXG9Bvs9G/giphy.gif" />
+        <h1 v-if="gameState.it === username">{{ gameState.countdown }}</h1>
+      </div>
+    </div>
+
+    <h1 class="loser" v-if="gameState && gameState.loser">LOSER: {{ gameState.loser }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000';
+const ME = 'jordoreed';
+const usernameParam = new URLSearchParams(document.location.search).get('username');
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      me: usernameParam || ME,
+      loading: true,
+      gameState: null,
+      usernames: []
+    };
+  },
+
+  created() {
+    this.load();
+  },
+
+  methods: {
+    load() {
+      console.log('loading app');
+
+      return this.checkin()
+        .then(this.getGameState)
+        .then(() => {
+          setInterval(this.update, 200);
+          this.loading = false;
+        })
+        .catch(error => {
+          console.log('error', error);
+        });
+    },
+
+    checkin() {
+      return axios.post(`${SERVER_URL}/api/game`, { username: this.me });
+    },
+
+    getGameState() {
+      console.log('getting game state');
+      return axios.get(`${SERVER_URL}/api/game`).then(response => {
+        this.gameState = response.data;
+        this.usernames = Object.keys(this.gameState.users).sort();
+        console.log('gamestate', this.gameState);
+      });
+    },
+
+    update() {
+      console.log('updating');
+      return this.checkin()
+        .then(this.getGameState);
+    },
+
+    pass(username) {
+      console.log('attempting to pass to', username);
+      if (this.gameState.it !== this.me) {
+        console.log('you are not it!');
+        return;
+      }
+
+      return axios.put(`${SERVER_URL}/api/game/it`, { username });
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.app {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.users {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+}
+
+.user img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  box-shadow: 0 3px 5px 0 rgba(0,0,0,0.5);
+}
+
+.user img:hover {
+  box-shadow: 0 5px 5px 0 rgba(0,0,0,0.5);
+  cursor: pointer;
+}
+
+.user div {
+  padding-top: 5px;
+}
+
+.loser {
+  color: red;
+	animation: flash linear 1s infinite;
+}
+
+@keyframes flash {
+	0% {
+    opacity: 1;
+    font-size: 100%;
+  }
+	50% {
+    opacity: .1;
+    font-size: 300%;
+  }
+	100% {
+    opacity: 1;
+    font-size: 100%;
+  }
 }
 </style>
